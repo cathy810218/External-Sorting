d05ac83c1b5c9352cd427bce3bd3e5fae2577043
cathy810218 <cathy810218@gmail.com>
2017-06-05 21:10:17 -0700

    Finish sorting files

--- src/main/java/com/codefellows/App.java
+++ src/main/java/com/codefellows/App.java
@@ -8,11 +8,13 @@ import com.google.code.externalsorting.ExternalSort;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws IOException {
         System.out.println( "Hello World!" );
-        String inputfile = "file1.txt";
-        String outputfile = "file2.txt";
+        
+        File inputfile = new File("/Users/cathyoun/IdeaProjects/java501externalsorting/src/main/resource/inputFile.txt");
+        File outputfile = new File("/Users/cathyoun/IdeaProjects/java501externalsorting/src/main/resource/outputFile.txt");
+
+        sort(inputfile, outputfile);
 
     }
 
